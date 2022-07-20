4209e2c0d065f2c899bc650c2dd1331002659738
stefan <stefan@eduroamnat32-36.eduroamnat.uv.es>
2022-07-20 11:56:46 +0200

    aux

---
+++ amiqsa/build/CMakeFiles/amiqs.exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/amiqs.exe.dir/amiqs.cpp.o"
+  "CMakeFiles/amiqs.exe.dir/amiqs.cpp.o.d"
+  "amiqs.exe"
+  "amiqs.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/amiqs.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ amiqsa/build/CMakeFiles/lsoda.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lsoda.dir/Source/LSODA.cpp.o"
+  "CMakeFiles/lsoda.dir/Source/LSODA.cpp.o.d"
+  "liblsoda.a"
+  "liblsoda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lsoda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
